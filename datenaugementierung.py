9870e025fa65c72155adcf888bd7707e43b09761
Anna Wiedenroth <awiedenroth@uos.de>
2022-02-23 18:27:37 +0100

    datenaugmentierung in progress

--- datenaugementierung.py
+++ datenaugementierung.py
@@ -4,6 +4,18 @@ from pathlib import Path
 from copy import deepcopy
 import fastText as fasttext
 import fasttext.util
+import nltk
+from nltk.tokenize import sent_tokenize, word_tokenize
+from nltk.corpus import stopwords
+from nltk.tokenize.treebank import TreebankWordDetokenizer
+import nlpaug.augmenter.char as nac
+import nlpaug.augmenter.word as naw
+import nlpaug.augmenter.sentence as nas
+
+
+nltk.download('stopwords')
+
+
 
 ft = fasttext.load_model('cc.de.300.bin')
 
@@ -41,20 +53,29 @@ class Augmentierer:
 
         return self.X_ft, self.y_ft
 
-def lowercase(string:str) -> str:
-    return string.lower()
+    def lowercase(string:str) -> str:
+        return string.lower()
 
-def remove_stopwords(string:str) -> str:
-    return [w for w in string if w not in stopwordList]
+    def remove_stopwords(string:str) -> str:
+        stopWords = set(stopwords.words('german'))
+        tokenized = word_tokenize(string)
+        clean = [w for w in tokenized if w not in stopWords]
+        return TreebankWordDetokenizer().detokenize([w for w in tokenized if w not in stopWords])
 
-def remove_numbers(string:str) -> str:
-    ...
+    def remove_numbers(string:str) -> str:
+        ...
 
-def remove_punctuation(string:str) -> str:
-    ...
+    def remove_punctuation(string:str) -> str:
+        ...
 
-def keyboard_augmentation(string:str) -> str:
-    ...
+    def keyboard_augmentation(string:str) -> str:
+        aug = nac.KeyboardAug()
+        augmented_text = aug.augment(string)
+        #print("Original:")
+        #print(string)
+        #print("Augmented Text:")
+        #print(augmented_text)
+        return augmented_text
 
-def synonym_augmentation(string:str) -> str:
-    ...
+    def synonym_augmentation(string:str) -> str:
+        ...
